fa48b2fb7e398e809cb72b361df0b201eb349f20
Elicia Au Duong <eliciaauduong@outlook.com>
2019-10-24 11:12:27 +1100

    week 4 sample code

---
+++ week-04/04-0-lecture/04-sample-code/strlen_array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int my_strlen(char *s);
+
+int main(void) {
+    int i = my_strlen("Hello Andrew");
+    printf("%d\n", i);
+    return 0;
+}
+
+int my_strlen(char *s) {
+    int length = 0;
+    while (s[length] != 0) {
+        length++;
+    }
+    return length;
+}
---
+++ week-04/04-0-lecture/04-sample-code/strlen_array.goto.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int my_strlen(char *s);
+
+int main(void) {
+    int i = my_strlen("Hello Andrew");
+    printf("%d\n", i);
+    return 0;
+}
+
+int my_strlen(char *s) {
+    int length = 0;
+loop:
+    if (s[length] == 0) goto end;
+       length++;
+    goto loop;
+end:
+    return length;
+}
