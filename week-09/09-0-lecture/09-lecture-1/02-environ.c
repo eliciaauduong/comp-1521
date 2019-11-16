6bf4306d9b314573141c5cfcf18794d6f63e7632
Elicia Au Duong <eliciaauduong@outlook.com>
2019-11-16 18:37:56 +1100

    week 9 lecture 1

---
+++ week-09/09-0-lecture/09-lecture-1/02-environ.c
@@ -0,0 +1,11 @@
+// print all environment variables
+ 
+#include <stdio.h>
+
+int main(void) {
+    extern char **environ;
+
+    for (int i = 0; environ[i] != NULL; i++) {
+        printf("%s\n", environ[i]);
+    }
+}
